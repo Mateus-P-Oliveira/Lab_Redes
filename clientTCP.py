beeec6b1d354728c81afa228300433c8363c68cd
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2024-04-08 17:39:38 +0000

    comentarios

--- clientTCP.py
+++ clientTCP.py
@@ -1,10 +1,17 @@
 from socket import *
 serverName = '0.0.0.0' # mudar depois Caso necessario 
 serverPort = 12000
+#Escolho tipo de conecçao como tendo so ip e porta e conexão bidirecional entre cliente e servidor
 clientSocket = socket(AF_INET, SOCK_STREAM)
+#Coloco o nome do servidor e a porta 
 clientSocket.connect((serverName,serverPort))
+#Insert the message 
 sentence = input('Input lowercase sentence:')
+#Envio a setença
 clientSocket.send(sentence.encode())
+#Recebo a stream
 modifiedSentence = clientSocket.recv(1024)
+#dou print no que recebi 
 print ('From Server:', modifiedSentence.decode())
+#Feço a conecção
 clientSocket.close()
--- serverTCP.py
+++ serverTCP.py
@@ -1,11 +1,16 @@
 from socket import *
+#Defino a porta do servidor
 serverPort = 12000
+#Crio o mesmo padrão de conecção do client
 serverSocket = socket(AF_INET,SOCK_STREAM)
+#Vincula o servidor a porta que escolhi
 serverSocket.bind(('',serverPort))
+#Torna o socket passivo na coneção
 serverSocket.listen(1)
 print('The server is ready to receive')
 while True:
     connectionSocket, addr = serverSocket.accept()
+    #deixa o server aguardando coneção
     sentence = connectionSocket.recv(1024).decode()
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence.encode())
