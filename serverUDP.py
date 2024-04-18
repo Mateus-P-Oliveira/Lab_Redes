7822df23577873fee9cc165615d5a80bc874ffdc
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2024-04-18 01:27:40 +0000

    Erro no segment message

--- serverUDP.py
+++ serverUDP.py
@@ -23,16 +23,24 @@ def broadcast():
         while not messages.empty(): #Enquanto nao esvaziar a fila 
             message, addr = messages.get() 
             print(message.decode())
+            
+            segmentMessage = message.decode('utf-8')
+            segmentMessage = segmentMessage.split("/")
+            print("____________")
+            print(segmentMessage[0]) #Ta tudo index zero
+            
             if addr not in clients: #Caso o endereço nao exista adiciona o usuario no servidor 
                 clients.append(addr)
             for client in clients: #Percorre a lista de clientes para enviar a mensagem
                 try:
+                    #if para os send
+                    #else essesaqui abaixo
                     if message.decode().startswith("SIGNUP_TAG:"): #Se começa com SIGNUP_TAG a mensagem de usuario joined é enviada
                         name = message.decode()[message.decode().index(":")+1:]
                         server.sendto(f"{name} joined!".encode(), client)
-                    elif message.decode().find("/send"):  #Mudar aqui pq a mensagem inclui o nome do usuario no padrão nome: /send 
-                        segmentMessage = message.decode.split("/")  
-                        print(segmentMessage[2])
+                    #elif message.decode().find("/send"):  #Mudar aqui pq a mensagem inclui o nome do usuario no padrão nome: /send 
+                       # segmentMessage = message.decode.split("/")  
+                       # print(segmentMessage[2])
                     else: #Caso não envia para todo mundo                      
                         server.sendto(message,client)
                 except:
