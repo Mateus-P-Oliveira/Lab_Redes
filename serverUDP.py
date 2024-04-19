50a4e6abc0186a941826a78f566de51513edf94d
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2024-04-19 19:15:54 +0000

    Erro ao adicionar client sem o for

--- serverUDP.py
+++ serverUDP.py
@@ -4,6 +4,7 @@ import queue
 
 messages = queue.Queue()
 clients = []
+aliases = []
 #Recebe o Socket
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("127.0.0.1",9999))
@@ -22,12 +23,15 @@ def broadcast():
     while True:
         while not messages.empty(): #Enquanto nao esvaziar a fila 
             message, addr = messages.get() 
-            print(message.decode())
-            
+            print(message.decode())            
             segmentMessage = message.decode('utf-8')
-            segmentMessage = segmentMessage.split("/")
-            print("____________")
-            print(segmentMessage[0]) #Ta tudo index zero
+            if "/" in segmentMessage:
+                segmentMessage = segmentMessage.split("/")
+                print("____________")
+                print(segmentMessage)  # print the whole list
+                print(segmentMessage[1]) 
+            else:
+                print("No '/' in the message")
             
             if addr not in clients: #Caso o endereço nao exista adiciona o usuario no servidor 
                 clients.append(addr)
@@ -38,10 +42,14 @@ def broadcast():
                     if message.decode().startswith("SIGNUP_TAG:"): #Se começa com SIGNUP_TAG a mensagem de usuario joined é enviada
                         name = message.decode()[message.decode().index(":")+1:]
                         server.sendto(f"{name} joined!".encode(), client)
-                    #elif message.decode().find("/send"):  #Mudar aqui pq a mensagem inclui o nome do usuario no padrão nome: /send 
+                        aliases.append(name)
+                    elif segmentMessage[1] == "send":  #Mudar aqui pq a mensagem inclui o nome do usuario no padrão nome: /send 
+                       print("Entrei")
+                                              
                        # segmentMessage = message.decode.split("/")  
                        # print(segmentMessage[2])
-                    else: #Caso não envia para todo mundo                      
+                    else: #Caso não envia para todo mundo
+                        print(aliases)                      
                         server.sendto(message,client)
                 except:
                    
